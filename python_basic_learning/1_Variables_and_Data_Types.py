550065ff33b8e697cc75ab611337705bb2e44f27
XianZS <xianzhisen_yang@outlook.com>
2026-01-25 16:41:36 +0800

    1.标量类型和容器类型 2.运算符的简单使用

--- python_basic_learning/1_Variables_and_Data_Types.py
+++ python_basic_learning/1_Variables_and_Data_Types.py
@@ -9,8 +9,92 @@
     （3）空
     None
 2.容器类型
-    （1）列表
-    （2）元组
-    （3）字典
-    （4）集合
+    （1）列表 list
+    （2）元组 tuple
+    （3）字典 dict
+    （4）集合 set
 """
+
+# 标量类型
+# int 整数
+int_value = 123
+print(int_value)
+# float 类型 浮点数 无double类型
+float_value = 123.4
+print(float_value)
+# 布尔数值 True or False
+bool_value_1 = True
+bool_value_2 = False
+print(bool_value_1, bool_value_2)
+# 复数类型：complex 1+2i
+complex_value = complex(1, 2)
+print(complex_value)
+# 针对一个变量，不知道变量是什么，那么可以调用type()方法
+print(type(complex_value), type(int_value))
+# str 字符串的数值不可变
+str_a = "abcdef1"
+print(str_a, type(str_a))
+# 将1改为g
+# str_a[6] = "g" 错误 不可以通过下标改变str的数值
+# print(str_a)
+# 下标从0开始 到len-1
+print(str_a[0], "=", str_a[1], "=", str_a[2])
+print(len(str_a))
+None_value = None
+print(None_value, type(None_value))
+
+# 列表 list
+nums_list = list()
+print(nums_list, type(nums_list))
+# 有序
+nums1 = [1, 2, 3]
+print(nums1[1], nums1[2], nums1[0])
+# 可变
+nums2 = [1, True, "abc"]
+print(nums2)
+# 可重复
+nums3 = [1, 1, 2, 2, 3]
+print(nums3)
+
+# 元组 tuple
+data = tuple()
+print(data, type(tuple))
+# 有序 可以通过下标访问
+data1 = (1, 2, 3)
+print(data1, data1[2], data1[0], data1[1])
+# 不可变 元素值不可改变
+# data2 = (1, 2, 3)
+# data2[2] = 100
+# print(data2)
+# 可重复 元素可以存在相同的值
+data3 = (1, 1, 1, 3, 3, 3, 2, 2, 2)
+print(data3)
+
+
+# 字典 dict key-value的组合
+some1 = dict()
+print(some1, type(some1))
+# 添加数值
+# some1["key"]=Value
+# dict={key1:value1,key2:value2,key3:value3}
+some2 = {"name": "jom", "age": 18}
+print(some2)
+# 通过key访问字典中的某个元素
+print(some2["age"])
+
+# 集合 set()
+some = set()
+print(some, type(some))
+# 无序
+some = {1, 2, 3, 4}
+print(some)
+# 可变
+some.add(5)
+print(some)
+# 无重复 自动进行去重处理
+news = {1, 1, 1, 2, 2, 2, 3, 3, 3}
+print(news)
+
+
+if __name__ == "__main__":
+    pass
--- python_basic_learning/2_Operators_and_Expressions.py
+++ python_basic_learning/2_Operators_and_Expressions.py
@@ -8,3 +8,93 @@
 5.成员运算符
 6.身份运算符
 """
+
+# 加、减、乘/整除、除、取模、幂运算
+a = 100
+b = 30
+c = 20
+print(a + b + c, a - b - c)
+print(a * b * c)
+print(a / b, a // b)
+print(a % b)
+print(a**2)
+# = += -= *= /=
+a %= b  # 相当于 a=a%b
+print(a)
+# > >= < <=
+if a > b:
+    print("a>b")
+else:
+    print("a<b")
+
+
+# and or not
+# 当逻辑条件成立时，那么结果是True，反之为False
+# 逻辑条件1 and 逻辑条件2 and 逻辑条件3
+def res1():
+    print("=== res1 ===")
+    return True
+
+
+def res2():
+    print("=== res2 ===")
+    return False
+
+
+# and 逻辑运算符:逻辑条件全部成立
+if res1() and res2():
+    print("res1 true res2 true")
+else:
+    print("res1 或 res2 存在False")
+
+print("=" * 20)
+
+# or 逻辑运算符:只要存在一个逻辑条件成立 只要有一个逻辑条件成立，那么该逻辑条件的后续逻辑条件都不会被执行
+if res1() or res2():
+    print("or 成立")
+else:
+    print("or 不成立")
+
+# not 逻辑运算符:取反;False的反就是True，相对的，True的反就是False
+if not res2():
+    print("not 成立")
+else:
+    print("not 不成立")
+
+# in 、 not in
+names = ["aom", "som", "dom"]
+people1 = "dom"
+people2 = "jack"
+# if 成员 in 数据集合:True
+# else: False
+if people1 in names:
+    print(f"{people1}在{names}之中")
+else:
+    print(f"{people1}不在{names}之中")
+
+if people2 in names:
+    print(f"[in]:{people2}在{names}之中")
+else:
+    print(f"[in]:{people2}不在{names}之中")
+
+if people2 not in names:
+    print(f"[not in]:{people2}不在{names}之中")
+else:
+    print(f"[not in]:{people2}在{names}之中")
+
+# is 、 not is
+# is 和 == 的区别？
+# is 比较的是对象的地址
+# == 比较的是对象的数值
+a = 1
+b = 1
+print(a == b)  # True
+print(a is b)  # True
+nums1 = [1, 2, 3]
+nums2 = [1, 2, 3]
+print(nums1 == nums2)  # True
+print(nums1 is nums2)  # False
+
+
+if __name__ == "__main__":
+    pass
