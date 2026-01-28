c3b7e195ed95a9bc22e31c15d1d7f60e5e3fa88b
XianZS <xianzhisen_yang@outlook.com>
2026-01-28 22:12:52 +0800

    2

--- python_basic_learning/4_Function_Basics.py
+++ python_basic_learning/4_Function_Basics.py
@@ -7,5 +7,176 @@
 4.函数的自定义文档帮助
 5.函数的作用域
 6.匿名函数（lambda 表达式）
-7.实战演示：简易计算器的制作
 """
+
+
+# 1.函数定义
+#   函数名+参数+函数体
+def add(a, b):
+    """
+    add:函数名（必选）
+    a，b：参数（可选）
+    return a+b：函数体（必选）
+    """
+    return a + b
+
+
+print(add(1, 2))
+
+
+# 2.函数参数
+#   （1）位置参数:需要按照顺序传入参数
+def add_1(a, b, c):
+    print(f"a:{a},b:{b},c:{c}")
+    return a + b + c
+
+
+# 1+2+3 a=1 b=2 c=3
+print(add_1(1, 2, 3))
+
+
+#   （2）关键字参数:直接指明字段所属的对象，就叫做关键字参数
+def some(name, age, details):
+    print(f"name:{name},age:{age},details:{details}")
+    return ""
+
+
+# 我不知道三个参数的顺序，但是我知道有三个参数
+some(name="jom", details="详细信息", age=19)
+
+
+#   （3）默认参数:参数传入的可选性，提前设定字段的内容，如果传入，就是用传入内容，如果没有传入，就使用默认设置的参数
+def some2(name, age, details="这是设置的默认参数"):  # 我可能不知道当前对象的详细信息
+    print(f"name:{name},age:{age},details:{details}")
+    return ""
+
+
+some2(name="jom", age=20, details="这是我传入的参数")
+some2(name="kom", age=19)
+
+
+#   （4）可变位置参数 *args：我不知道应该传入多少个参数，传入参数的个数不确定的情况
+#   位置参数，会按照你传入参数的顺序，将他们封装成一个元组对象
+#   *args
+# def some3(a, b, c, d, e, f, g):
+def some3(*args):
+    print(args)
+    # print(f"{a},{b},{c},{d},{e},{f},{g}")
+    return ""
+
+
+some3(1, 2, 3, 4, 5)
+some3(1, 2, 3, 4, 5, 6)
+some3()
+
+
+#   （5）可变关键字参数 **kwargs:我也不知道要传入多少个键值对，以k-v的形式传入参数
+#   将传入的内容，封装成一个dict格式
+def some4(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+    return ""
+
+
+# 封装一个学生信息
+some4(stu_name="jom", stu_age="19", stu_details="暂无详细信息")
+some4(teacher_name="kom", stu_age="32")
+
+
+# 3.函数返回值：return 后面的内容就是函数的返回值
+#   （1）单返回值
+def add_2(a, b):
+    return a + b
+
+
+print(add_2(1, 2))
+
+
+#   （2）多返回值:多返回值的类型是元组，会将多个返回值封装到一个元组之中
+def some5(child1, child2, child3):
+    return child1, child2
+
+
+print(some5(1, 2, 3))
+
+
+# 4.函数的自定义文档帮助：面向共同开发者，解释自己的函数功能
+def some6(a, b, c):
+    """
+    函数自定义文档
+    - 需要指明函数名：some6
+    - 需要指明传入参数：a:int，b:int，c:int
+    - 需要指明函数的大致功能：返回大于5的参数
+    """
+    res = []
+    if a > 5:
+        res.append(a)
+    if b > 5:
+        res.append(b)
+    if c > 5:
+        res.append(c)
+    return res
+
+
+some6(3, 6, 10)
+help(some6)
+
+
+#   help帮助文档
+# 5.函数的作用域
+#   （1）全局变量
+#   - 可以读：可以在函数内部调到（read）全局变量
+#   - 不可以写：不可以修改全局变量
+a = 1
+
+
+def some7():
+    print(a)
+    # a = 2
+    return ""
+
+
+some7()
+
+
+#   （2）局部变量
+#   在函数内部定义的变量，就是当前函数的局部变量，局部变脸具有不可互通性质
+#   不可以在全局之中，访问局部变量
+def main():
+    a = 1
+    b = 2
+    print(a + b)
+    return ""
+
+
+main()
+
+
+#   （3）转换：在函数内容，修改全局变量，使用global参数修饰全局变量
+b = 1
+
+
+def some8():
+    global b
+    b = 2
+    print(b)
+    return ""
+
+
+some8()
+print("===", b)
+
+
+# 6.匿名函数
+#   lambda 参数列表：表达式
+# c=lambda a,b:a+b
+x = 2
+y = 5
+number = lambda x, y: x**y
+print(number)
+print(number(x, y))
+
+some = [("jom", 89), ("kom", 99), ("lom", 32), ("fom", 55)]  # type:ignore
+print("现在将要按照成绩排序：", some)
+some.sort(key=lambda x: x[1])  # type:ignore
+print("按照成绩排序之后的结果为：", some)
--- python_basic_learning/5_container_structure.py
+++ python_basic_learning/5_container_structure.py
@@ -1,8 +1,125 @@
-"""
-# 容器类型
+"""=== 容器类型 ==="""
 
-1.列表
-2.元组
-3.字典
-4.集合
-"""
+# 1.列表 list
+# 定义
+alist = list()
+blist = [1, 2, 3, 4, 5, 6]
+print(alist, blist)
+# 查找:按照下标查找元素，按照元素查找下标
+index_2_value = blist[2]
+print(index_2_value)
+# 增加:在末尾增加元素，或者在任意位置增加元素
+clist = [1, 2, 4, 5, 6]
+# O（1）在末尾增加元素使用append(增加元素对象)
+clist.append(7)
+print("append >>> ", clist)
+# O（n）在任意位置添加元素使用insert(添加位置，添加元素对象)
+clist.insert(2, 3)
+print("insert >>> ", clist)
+# 列表扩展 目标对象1.extend(目标对象2) 将目标对象2先拆解，然后逐个添加到目标对象1之后
+dlist = [1, 2, 3]
+elist = [4, 5, 6]
+dlist.extend(elist)
+print(dlist)
+# 修改 根据下标修改元素
+flist = [1, 2, 3, 0, 5, 6]
+flist[3] = 4
+print("update:", flist)
+# 删除 （1）.按照索引删除 （2）按照值对象删除 （3）删除末尾最后的一个元素
+print(f"flist删除之前为:{flist}")
+del flist[4]
+print(f"flist删除之后为:{flist}")
+flist.remove(6)
+print(f"remove 之后的结果为:{flist}")
+res = flist.pop()
+print(f"pop value is {res},pop之后的结果为:{flist}")
+# 常用方法：切片，统计个数、反转、排序
+glist = [12, 3, 4, 52, 3, 5, 1, 2, 5, 12, 5, 12, 241, 34123]
+# 得到index=1 to index=6 结束之前的元素 [起始下标:终止下标]
+split_obj = glist[1:7]
+print(split_obj)
+# 统计个数 被统计对象.count(被统计元素)
+c5 = glist.count(5)
+print(f"在{glist}之中存在{c5}个5")
+# reverse 反转函数
+hlist = [3, 2, 1]
+hlist.reverse()
+print("排序之前的结果为:", glist)
+glist.sort(key=lambda x: x, reverse=True)
+print(glist)
+print("===" * 30)
+# 2.元组 tuple
+# 定义
+atuple = tuple()
+btuple = (1, 2, 3, 4, 5, 1)
+print(atuple, btuple)
+# 查找 按照下标查找
+index_4_value = btuple[4]
+print(index_4_value)
+# 常用方法：切片，统计
+some = btuple[1:4]
+print("切片结果为:", some)
+print("1元素的个数为:", btuple.count(1))
+print("===" * 30)
+# 3.字典 dict
+# 定义
+adict = dict()
+bdict = {"name": "jom", "age": 18}
+print(adict, bdict)
+# 查找 列表和元组查找元素，通常通过下标查找元素，但字典不同，字典通过key查找
+name = bdict["name"]  # 字典对象["key"] 》》》 key对应的value
+print(f"name is {name}")
+# 增加
+bdict["details"] = "详细信息"  # 字典对象["增加的key"]="增加的key对应的value"
+print(bdict)
+# 修改:通过key进行修改
+bdict["details"] = "这是修改之后的结果"
+print(bdict)
+# 删除
+del bdict["name"]
+print(bdict)
+# 常用方法：获取所有键、获取所有值，获取所有键值对
+# 获取所有的键 keys()
+keys = bdict.keys()
+print(f"bdict字典的所有key:{keys}")
+# 获取所有的值 values()
+values = bdict.values()
+print(f"bdict字典的所有value:{values}")
+# 获取所有的键值对 items()
+items = bdict.items()
+print(f"bdict字典的所有键值对:{items}")
+for key, value in bdict.items():
+    print(key, value)
+print("===" * 30)
+# 4.集合 set
+# 定义
+aset = set()
+bset = {1, 2, 3, 4, 5, 6, 7, 8, 9, 1}
+print(aset, bset)  # 对元素进行自动去重处理
+# 查找 无索引结构，不可以根据下标查找
+# 无序性的体现
+cset = set()
+cset.add(1)
+cset.add(5)
+cset.add(99)
+print(cset)
+print(5 in cset)
+print(2 in cset)
+# 增加 在列表之中，在末尾增加元素使用append(添加元素对象)，在集合之中，在末尾添加元素使用add(添加元素对象)
+cset.add(0)
+print(cset)
+# 删除：针对集合删除，使用数值对应删除remove
+cset.remove(1)
+print(cset)
+# 常用方法：常见的集合运算：交集 并集 差集 对称差集
+dset = {1, 2, 3, 4, 5, 6}
+eset = {4, 5, 6, 7, 8, 9}
+print(f"交集运算(&):{dset & eset}")  # 哪些元素同时在d之中，也在e之中
+print(f"并集运算(|):{dset | eset}")  # d和e的所有元素
+print(f"差集运算(-):{dset - eset}")  # dset中去除d和e的共同值之后，还剩下哪些元素
+print(f"差集运算(-):{eset - dset}")
+print(f"对称差集运算(^):{dset ^ eset}")  # 对称差集运算
+#
+#
+#
+#
